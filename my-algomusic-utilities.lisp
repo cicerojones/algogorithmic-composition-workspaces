db8fa1a1e62ea1bc45a47e51a603a30dea1dee06
Will Orzo <stevejobsss3000@gmail.com>
2016-08-23 11:05:45 -0400

    remove all source code from top-level

--- my-algomusic-utilities.lisp
+++
@@ -1,8 +0,0 @@
-;;; used in an open music workspace
-(in-package :om)
-
-(defun mapping-random (lst) 
-  (let ((n (random 6)))
-    (mapcar (lambda (x) (* 100 (+ 60 (+ 1 n x)))) lst)))
-
-;; (lambda (lols) (mapcar #'mapping-random lols))
