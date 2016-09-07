205240ee6b83d1ef36b950f4333b20acf49450b4
Will Orzo <stevejobsss3000@gmail.com>
2016-09-07 17:16:52 -0400

    add new files and documentation for rtm-dbs

---
+++ collection-of-om-workspaces/august25-workspace/wsparams.lisp
@@ -0,0 +1,1 @@
+(in-package :om)(setf *ws-params* (list 1 (om-make-point 20 50) (om-make-point 280 480)))
--- pwgl-files/berkowitz-and-other-duets/rtm-database.lisp
+++ pwgl-files/berkowitz-and-other-duets/rtm-database.lisp
@@ -1,12 +1,12 @@
 (
  ;; single quarter note in 1/4
- ((1 (1)))
+ ((1 (1)))				
  ;; half-note in 2/4
  ((2 (1)))
  ;; dotted-half in 3/4
  ((3 (1)))
  ;; whole-note in 4/4
- ((4 (1))) 				
+ ((4 (1))) 				; 4
  ;; two eighths in 1/4
  ((1 (1 1)))				
  ;; two quarters in 2/4
@@ -14,7 +14,7 @@
  ;; weird quarter-note duplet in 3/4
  ((3 (1 1)))
  ;; weird two half note duplet in 4/4
- ((4 (1 1)))
+ ((4 (1 1)))				;22
  ;; three eighth-note triplet
  ((1 (1 1 1)))
  ;; quarter-note triplet in 3/4
@@ -22,7 +22,7 @@
  ;; three quarters in 3/4
  ((3 (1 1 1)))
  ;; quarter-note triplet in 4/4
- ((4 (1 1 1)))
+ ((4 (1 1 1)))				
  ;; 4 sixteenths in 1/4
  ((1 (1 1 1 1)))
  ;; 4 eighths in 2/4
@@ -30,28 +30,29 @@
  ;; 4 quarter 'quadruplet' in 3/4
  ((3 (1 1 1 1)))
  ;; 4 quarters in 4/4
- ((4 (1 1 1 1)))
- ;;;;
+ ((4 (1 1 1 1))) 			;1111
+
+ ;;;; all 4/4
  ;; two half-notes in 4/4
- ((2 (1)) (2 (1)))
+ ((2 (1)) (2 (1)))			;22better
  ;; dotted-half, quarter in 4/4
- ((3 (1)) (1 (1)))
+ ((3 (1)) (1 (1)))			;31
  ;; quarter, dotted-half in 4/4
- ((1 (1)) (3 (1)))
+ ((1 (1)) (3 (1))) 			;13
  ;; half, two quarters in 4/4
- ((2 (1)) (1 (1)) (1 (1)))
+ ((2 (1)) (1 (1)) (1 (1)))		;211
  ;; quarter, half, quarter in 4/4
- ((1 (1)) (2 (1)) (1 (1)))
+ ((1 (1)) (2 (1)) (1 (1)))		;121
  ;; two quarters, half in 4/4
- ((1 (1)) (1 (1)) (2 (1)))
+ ((1 (1)) (1 (1)) (2 (1)))		;112
  ;; four quarters in 4/4 (concisest)
- ((1 (1)) (1 (1)) (1 (1)) (1 (1)))
+ ((1 (1)) (1 (1)) (1 (1)) (1 (1)))	;1111best
 
  ;;;; some gaps in display
  ;; two quarters, half in 4/4
- ((2 (1 1)) (2 (1)))
+ ((2 (1 1)) (2 (1))) 
  ;; half, two quarters in 4/4
- ((2 (1)) (2 (1 1)))
+ ((2 (1)) (2 (1 1))) 
 
  ;; four eighths, half in 4/4
  ((2 (1 1 1 1)) (2 (1)))
